4d07fe9155e09e7ffbe0f6c4dac01343e111c61d
carolinecamilo <carolinecamilo713@gmail.com>
2026-05-13 22:34:45 -0300

    Add files via upload

---
+++ Aula - JavaS/javascript.js
@@ -0,0 +1,27 @@
+var x = 10
+if (x > 5) {
+    console.log ( " x is greater than 5");
+} else if (  x > 5) {
+    console.log ( x is less than 5)
+} else {
+    console.log ( " x is equal to 5"); "X is equal 5"
+}
+
+
+for (var i = 0; i < 10; i ++ ) {
+    if ( i % 2 == 0)
+        console.log (i); 0,2,4,,6,8
+: 
+}
+
+for (let i = 0; 1 += 2)  {
+    console.log(i); 0,2,4,6,8
+};
+for (let i = 1; < 10; i +=){
+    console.log (i)1,3,5,7,9
+};
+var y = 0;
+while (y < 10) {
+    console.log (y); 0,1,2,3,4,5,6,7,8,9
+    y++;
+}
