55564be730a3253fc386cd69b1c32bab516c85ab
carolinecamilo <carolinecamilo713@gmail.com>
2026-05-19 22:29:17 -0300

    Add files via upload

---
+++ Aula - javascript/aula14-script.js
@@ -0,0 +1,26 @@
+var issoEumaString = "isso é uma string";
+let issoEumNumero = 42;
+
+
+console.log ("As strins são diferentes")
+
+
+console.log(issoEumNumero + IssotambemEumaString); '4242'
+
+console.log (issoeumNumero + issoEumNumero)
+
+
+
+for (let i = 0; i <5; i++ ) {
+  console.log(i); 0, 1, 2, 3, 4
+  console.log(i*2); 1, 3, 5, 7, 9
+}
+
+//imprimindo os numeros pares com whilw
+let j = 0;
+while (j < 5 ) {
+    console.log (j*2+1); 1, 3, 5, 7, 9
+    j++;
+}
+
+console.log(Math.PI)
